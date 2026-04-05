00c01854b002b0cadcbbfa2690c4110f86aa481f
beomjunchoe <saintcamel@naver.com>
2026-04-05 12:42:24 +0900

    Add branded app icon

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -13,11 +13,8 @@ export default function AppleIcon() {
       <div
         style={{
           alignItems: "center",
-          background:
-            "linear-gradient(180deg, rgb(251, 247, 239) 0%, rgb(241, 234, 216) 100%)",
-          color: "#13221d",
+          background: "#f4efe4",
           display: "flex",
-          flexDirection: "column",
           height: "100%",
           justifyContent: "center",
           width: "100%",
@@ -26,18 +23,108 @@ export default function AppleIcon() {
         <div
           style={{
             alignItems: "center",
-            background: "#ec6c2f",
-            borderRadius: 32,
-            color: "white",
+            background: "#13221d",
+            borderRadius: 42,
             display: "flex",
-            fontSize: 74,
-            fontWeight: 700,
-            height: 108,
+            height: 146,
             justifyContent: "center",
-            width: 108,
+            width: 146,
           }}
         >
-          3-1
+          <div
+            style={{
+              alignItems: "center",
+              background: "#fff9f0",
+              borderRadius: 34,
+              display: "flex",
+              height: 118,
+              justifyContent: "center",
+              position: "relative",
+              width: 118,
+            }}
+          >
+            <div
+              style={{
+                background: "#ec6c2f",
+                borderRadius: 26,
+                display: "flex",
+                height: 94,
+                position: "relative",
+                width: 94,
+              }}
+            >
+              <div
+                style={{
+                  alignItems: "center",
+                  background: "#fff7ee",
+                  borderRadius: 16,
+                  display: "flex",
+                  height: 42,
+                  justifyContent: "center",
+                  left: 10,
+                  position: "absolute",
+                  top: 14,
+                  width: 42,
+                }}
+              >
+                <div
+                  style={{
+                    borderBottom: "6px solid #ec6c2f",
+                    borderLeft: "6px solid #ec6c2f",
+                    height: 14,
+                    transform: "rotate(-45deg)",
+                    width: 22,
+                  }}
+                />
+              </div>
+              <div
+                style={{
+                  color: "#fff7ee",
+                  fontSize: 24,
+                  fontWeight: 700,
+                  left: 56,
+                  lineHeight: 1,
+                  position: "absolute",
+                  top: 16,
+                }}
+              >
+                3-1
+              </div>
+              <div
+                style={{
+                  background: "#fff7ee",
+                  borderRadius: 12,
+                  bottom: 12,
+                  height: 18,
+                  left: 10,
+                  position: "absolute",
+                  width: 30,
+                }}
+              />
+              <div
+                style={{
+                  background: "#ffd8c5",
+                  borderRadius: 12,
+                  bottom: 12,
+                  height: 18,
+                  left: 44,
+                  position: "absolute",
+                  width: 36,
+                }}
+              />
+              <div
+                style={{
+                  background: "#ffd449",
+                  borderRadius: 999,
+                  height: 16,
+                  position: "absolute",
+                  right: 9,
+                  top: 8,
+                  width: 16,
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     ),
--- app/icon.tsx
+++ app/icon.tsx
@@ -13,11 +13,8 @@ export default function Icon() {
       <div
         style={{
           alignItems: "center",
-          background:
-            "linear-gradient(180deg, rgb(251, 247, 239) 0%, rgb(241, 234, 216) 100%)",
-          color: "#13221d",
+          background: "#f4efe4",
           display: "flex",
-          flexDirection: "column",
           height: "100%",
           justifyContent: "center",
           width: "100%",
@@ -26,18 +23,108 @@ export default function Icon() {
         <div
           style={{
             alignItems: "center",
-            background: "#ec6c2f",
-            borderRadius: 36,
-            color: "white",
+            background: "#13221d",
+            borderRadius: 44,
             display: "flex",
-            fontSize: 88,
-            fontWeight: 700,
-            height: 116,
+            height: 152,
             justifyContent: "center",
-            width: 116,
+            width: 152,
           }}
         >
-          3-1
+          <div
+            style={{
+              alignItems: "center",
+              background: "#fff9f0",
+              borderRadius: 36,
+              display: "flex",
+              height: 124,
+              justifyContent: "center",
+              position: "relative",
+              width: 124,
+            }}
+          >
+            <div
+              style={{
+                background: "#ec6c2f",
+                borderRadius: 28,
+                display: "flex",
+                height: 98,
+                position: "relative",
+                width: 98,
+              }}
+            >
+              <div
+                style={{
+                  alignItems: "center",
+                  background: "#fff7ee",
+                  borderRadius: 16,
+                  display: "flex",
+                  height: 44,
+                  justifyContent: "center",
+                  left: 12,
+                  position: "absolute",
+                  top: 14,
+                  width: 44,
+                }}
+              >
+                <div
+                  style={{
+                    borderBottom: "6px solid #ec6c2f",
+                    borderLeft: "6px solid #ec6c2f",
+                    height: 14,
+                    transform: "rotate(-45deg)",
+                    width: 24,
+                  }}
+                />
+              </div>
+              <div
+                style={{
+                  color: "#fff7ee",
+                  fontSize: 28,
+                  fontWeight: 700,
+                  left: 60,
+                  lineHeight: 1,
+                  position: "absolute",
+                  top: 16,
+                }}
+              >
+                3-1
+              </div>
+              <div
+                style={{
+                  background: "#fff7ee",
+                  borderRadius: 12,
+                  bottom: 12,
+                  height: 20,
+                  left: 12,
+                  position: "absolute",
+                  width: 32,
+                }}
+              />
+              <div
+                style={{
+                  background: "#ffd8c5",
+                  borderRadius: 12,
+                  bottom: 12,
+                  height: 20,
+                  left: 48,
+                  position: "absolute",
+                  width: 38,
+                }}
+              />
+              <div
+                style={{
+                  background: "#ffd449",
+                  borderRadius: 999,
+                  height: 18,
+                  position: "absolute",
+                  right: 10,
+                  top: 8,
+                  width: 18,
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     ),
